6840a3f1c7d6011931abc2ae50f82ea43bc409b1
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-04-01 21:41:16 -0300

    Add Gerente class extending Login

---
+++ Gerente.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio01;
+
+/**
+ *
+ * @author aluno9
+ */
+public class Gerente extends Login{
+     
+     
+}
