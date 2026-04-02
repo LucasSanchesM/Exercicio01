7b20cb0e1eded5cb59c4bd2fe9e6496dab54eb4e
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-04-01 21:40:39 -0300

    Add Login class with id and senha properties

---
+++ Login.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio01;
+
+/**
+ *
+ * @author aluno9
+ */
+public class Login {
+    private int id;
+    private int senha;
+    
+    public void setId(int entraId){
+        this.id = entraId;
+    }
+    public void setSenha(int entraSenha){
+        this.senha = entraSenha;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public int getSenha(){
+        return this.senha;
+    }
+}
